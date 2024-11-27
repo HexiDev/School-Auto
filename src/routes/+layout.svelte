6b9f2fb768d46f81a35e1f66cd95cf0448056447
HexiDev <yourioldenzeel08@gmail.com>
2024-11-27 15:07:51 +0100

    Homepage, simpele achtergrond en bezig met devicemodal

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import "./styles.css";
+  let { children } = $props();
+</script>
+
+<div class="containerLayout">
+  {@render children()}
+</div>
---
+++ src/routes/components/DeviceModal.svelte
@@ -0,0 +1,16 @@
+<script>
+  // your script goes here
+</script>
+
+<main>
+  <div
+    class="overlay"
+    on:click={() => {
+      alert("click");
+    }}
+    on:keydown={() => {}}
+    aria-hidden={true}
+  ></div>
+</main>
+
+<style></style>
